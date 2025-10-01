19f025223143f47f505fa0d780138cb866683cf3
Sakshamgoyal02 <sakshamgoyal142@gmail.com>
2025-10-01 17:46:16 +0530

    deletePost

---
+++ BlogPosts.js
@@ -0,0 +1,20 @@
+module.exports= [
+     {
+        id: 1,
+        "title": "Heroes",
+        "email": "Heroes@gmail.com",
+        "author": "Ironman"
+     },
+    
+      {  id: 2,
+        "title": "Villains",
+        "email": "Villains@gmail.com",
+        "author": "Thanos"
+     },
+
+      { id: 3,
+        "title": "Civilians",
+        "email": "Civilians@gmail.com",
+        "author": "NewYork Times"
+     }
+]
